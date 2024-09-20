e174d1fb5f3c7c560b7196b94ccaec220e3a3ffa
Santiago <santiago.baillo@fi365.ort.edu.uy>
2024-09-19 22:06:49 -0300

    Agregada herencia en Agencias

---
+++ Dominio/Extranjera.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Extranjera : Agencia
+    {
+        private int _calificacion;
+
+        public Extranjera(string nombre, string pais, int calificacion):base(nombre,pais)
+        {
+            _calificacion = calificacion;
+        }
+
+        private void ValidarCalificacion()
+        {
+            if (_calificacion < 1 || _calificacion > 5) throw new Exception("La calificacion debe estar entre 1 y 5");
+        }
+
+        public override void Validar()
+        {
+            base.Validar();
+            ValidarCalificacion();
+        }
+    }
+}
---
+++ Dominio/Nacional.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Nacional : Agencia
+    {
+        private string _rut;
+        private int _anio;
+
+        public Nacional(string nombre, string pais, string rut, int anio):base(nombre, pais)
+        {
+            _rut = rut;
+            _anio = anio;
+        }
+
+        public override void Validar()
+        {
+            base.Validar();
+            if (string.IsNullOrEmpty(_rut)) throw new Exception("El RUT no puede ser vacio");
+            if (_anio < 1900) throw new Exception("El año debe ser mayor a 1900");
+        }
+    }
+}
